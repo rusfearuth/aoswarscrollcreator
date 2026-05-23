1e4cdb254137919c0cdb7056b9830e5a94238448
Alexander Ustinov <rusfearuth@gmail.com>
2026-05-23 20:08:39 +0700

    feat: Fixes metrica

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { yandexMetrikaHead } from "./src/analytics/yandexMetrika";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/aoswarscrollcreator/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "inject-yandex-metrika",
+      transformIndexHtml(html) {
+        return html.replace("</head>", `${yandexMetrikaHead}</head>`);
+      },
+    },
+  ],
 });
